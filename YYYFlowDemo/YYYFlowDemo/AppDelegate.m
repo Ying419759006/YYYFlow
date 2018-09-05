04506b6d6ed74e6201013a2172e32877e2992d9f
yingzhaoj <zhaoying9006@qq.com>
2018-09-05 10:48:50 +0800

    升级到1.0.1版本

--- YYYFlowDemo/YYYFlowDemo/AppDelegate.m
+++ YYYFlowDemo/YYYFlowDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"版本升级到1.0.1版本");
     return YES;
 }
 
